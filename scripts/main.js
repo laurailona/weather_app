cf292d9ebb9136484c07a81aa12954874fe64389
Laura Ilona <clarkelaura92@gmail.com>
2018-01-13 19:32:17 +0200

    Add jQuery, get basic data from FCC weather API

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  const cityDisplay = document.getElementById("city-name");
+  const weatherDisplay = document.getElementById("weather");
+  const iconDisplay = document.getElementById("weather-icon");
+
+  let weather;
+  let temperature;
+  let humidity;
+  let city;
+  let iconUrl;
+
+  if ("geolocation" in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition(function(position) {
+          let latitude = position.coords.latitude;
+          let longitude = position.coords.longitude;
+          let url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+          $.getJSON(url, function(data) {
+            console.log(data);
+            weather = data.weather[0].description;
+            temperature = data.main.temp;
+            humidity = data.main.humidity;
+            city = data.name;
+            iconUrl = data.weather[0].icon;
+            console.log("You are currently in " + city);
+            console.log("WEATHER: " + weather + ", TEMPERATURE: " + temperature + ", HUMIDITY: " + humidity);
+
+            cityDisplay.textContent = city;
+            weatherDisplay.textContent = "WEATHER: " + weather + ", TEMPERATURE: " + temperature + ", HUMIDITY: " + humidity;
+            iconDisplay.setAttribute("src", iconUrl);
+        });
+      });
+  } 
+  else {
+      /* geolocation IS NOT available */
+  }
+
+});
